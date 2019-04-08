d9ab1f6f1624eb6f180bfc3337be13ef6ff1506c
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-04-08 05:25:07 -0300

    Add initial test, package, and source code files

---
+++ __tests__/main.test.js
@@ -0,0 +1,8 @@
+const { _ } = require('higher-order-.js');
+
+describe('_.map', () => {
+    test('it maps every item in the provided array and returns the mapped output', () => {
+
+    });
+});
+
---
+++ higher-order.js
@@ -0,0 +1,9 @@
+
+/**
+ * The root object of the library containing the higher-order functions.
+ */
+const _ = {
+
+}
+
+module.exports = { _ };
