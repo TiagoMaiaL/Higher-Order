964834f769e38e7f0dbdddfe28020230357fa776
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-04-08 06:02:29 -0300

    Add initial definition of the map function of the library

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,16 +1,20 @@
-const { _ } = require('higher-order-.js');
+const { _ } = require('../higher-order.js');
 
 describe('_.map', () => {
-    test('maps an empty array and returns an empty array object', () => {
-
-    });
-
     test('throws a typeError if the provided object to be mapped isn\'t an array', () => {
 
     });
 
     test('throws a typeError if the provided mapper isn\'t a function value', () => {
 
+    });
+
+    test('maps an empty array and returns an empty array object', () => {
+        let input = [];
+        let inputMapper = () => {};
+        let expectedOutput = [];
+
+        expect(_.map(input, inputMapper)).toEqual(expectedOutput);
     });
 
     test('maps every item in the provided array and returns the mapped output', () => {
--- higher-order.js
+++ higher-order.js
@@ -3,7 +3,15 @@
  * The root object of the library containing the higher-order functions.
  */
 const _ = {
-
+    /**
+     * Given an array and a mapper closure, returns a transformed array based the mapper being applied to each original array elements.
+     * @param {Array} arr - The array to be mapped into another array of mapped values. 
+     * @param {Function} mapper - The function being applied to map each value of the original array.
+     * @returns {Array} mappedArray - The mapped array.
+     */
+    map: function(arr, mapper) {
+        return [];
+    }
 }
 
 module.exports = { _ };
